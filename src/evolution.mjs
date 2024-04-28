f1f3b010707e0d873823e2321518d10f4e4985e5
GlassB3ad <j.lahtivirta@gmail.com>
2024-04-28 15:41:16 +0300

    refactored

--- src/evolution.mjs
+++ src/evolution.mjs
@@ -1,14 +1,6 @@
 import { Pattern } from "./pattern.mjs"
 
-export const countLivingNeighbours = (cell, pattern) => [
-    pattern.isAlive(cell.leftNeighbour()),
-    pattern.isAlive(cell.leftUpNeighbour()),
-    pattern.isAlive(cell.rightNeighbour()),
-    pattern.isAlive(cell.rightDownNeighbour()),
-    pattern.isAlive(cell.downNeighbour()),
-    pattern.isAlive(cell.leftDownNeighbour()),
-    pattern.isAlive(cell.upNeighbour()),
-    pattern.isAlive(cell.rightUpNeighbour())]
+export const countLivingNeighbours = (cell, pattern) => cell.getAllNeighbours().map(cell => pattern.isAlive(cell))
     .reduce((acc, cur) => cur ? acc + 1 : acc, 0)
 
 export const stayAlive = (cell, pattern) => {

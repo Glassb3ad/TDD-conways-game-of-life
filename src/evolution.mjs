7a692ca8e808cfab4f9d6f8cef04bf30c0df5b61
GlassB3ad <j.lahtivirta@gmail.com>
2024-04-28 14:48:15 +0300

    evolution function and refactored

---
+++ src/evolution.mjs
@@ -0,0 +1,25 @@
+export const countLivingNeighbours = (cell, pattern) => {
+    let livingNeighbours = 0
+    if (pattern.isAlive(cell.leftNeighbour())) livingNeighbours++;
+    if (pattern.isAlive(cell.leftUpNeighbour())) livingNeighbours++;
+    if (pattern.isAlive(cell.rightNeighbour())) livingNeighbours++;
+    if (pattern.isAlive(cell.rightDownNeighbour())) livingNeighbours++;
+    if (pattern.isAlive(cell.downNeighbour())) livingNeighbours++;
+    if (pattern.isAlive(cell.leftDownNeighbour())) livingNeighbours++;
+    if (pattern.isAlive(cell.upNeighbour())) livingNeighbours++;
+    if (pattern.isAlive(cell.rightUpNeighbour())) livingNeighbours++;
+    return livingNeighbours
+}
+
+export const stayAlive = (cell, pattern) => {
+    let livingNeighbours = 0
+    if (pattern.isAlive(cell.leftNeighbour())) livingNeighbours++;
+    if (pattern.isAlive(cell.leftUpNeighbour())) livingNeighbours++;
+    if (pattern.isAlive(cell.rightNeighbour())) livingNeighbours++;
+    if (pattern.isAlive(cell.rightDownNeighbour())) livingNeighbours++;
+    if (pattern.isAlive(cell.downNeighbour())) livingNeighbours++;
+    if (pattern.isAlive(cell.leftDownNeighbour())) livingNeighbours++;
+    if (pattern.isAlive(cell.upNeighbour())) livingNeighbours++;
+    if (pattern.isAlive(cell.rightUpNeighbour())) livingNeighbours++;
+    return livingNeighbours === 2
+}
---
+++ test/evolution.test.mjs
@@ -0,0 +1,46 @@
+import { describe, test } from "vitest";
+import { expect } from "chai";
+import { Pattern } from "../src/pattern.mjs";
+import { Cell } from "../src/cell.mjs";
+import { stayAlive, countLivingNeighbours } from "../src/evolution.mjs";
+
+
+describe("Test evolution", () => {
+    test("Count that number of living neighbours is 0", () => {
+        const pattern = new Pattern()
+        const cell = new Cell(0, 0)
+        expect(countLivingNeighbours(cell, pattern)).toBe(0)
+    });
+
+    test("Count that number of living neighbours is 3", () => {
+        const pattern = new Pattern()
+        const cell = new Cell(0, 0)
+        pattern.add(cell.downNeighbour())
+        pattern.add(cell.leftNeighbour())
+        pattern.add(cell.rightDownNeighbour())
+        expect(countLivingNeighbours(cell, pattern)).toBe(3)
+    });
+
+    test("Count that number of living neighbours is 1", () => {
+        const pattern = new Pattern()
+        const cell = new Cell(0, 0)
+        const notNeighbour = new Cell(2, 2)
+        pattern.add(notNeighbour)
+        pattern.add(cell.downNeighbour())
+        expect(countLivingNeighbours(cell, pattern)).toBe(1)
+    });
+
+    test("Cell with 0 alive neighbours dies", () => {
+        const pattern = new Pattern()
+        const cell = new Cell(0, 0)
+        expect(stayAlive(cell, pattern)).toBe(false)
+    });
+
+    test("Cell with exactly 2 alive neighbours stays alive", () => {
+        const pattern = new Pattern()
+        const cell = new Cell(0, 0)
+        pattern.add(cell.downNeighbour())
+        pattern.add(cell.leftNeighbour())
+        expect(stayAlive(cell, pattern)).toBe(true)
+    });
+});

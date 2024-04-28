7cb5fd3f2715f25aab6b37180faffdab23b4b64c
GlassB3ad <j.lahtivirta@gmail.com>
2024-04-28 14:53:49 +0300

    refactored

--- src/evolution.mjs
+++ src/evolution.mjs
@@ -1,15 +1,13 @@
-export const countLivingNeighbours = (cell, pattern) => {
-    let livingNeighbours = 0
-    if (pattern.isAlive(cell.leftNeighbour())) livingNeighbours++;
-    if (pattern.isAlive(cell.leftUpNeighbour())) livingNeighbours++;
-    if (pattern.isAlive(cell.rightNeighbour())) livingNeighbours++;
-    if (pattern.isAlive(cell.rightDownNeighbour())) livingNeighbours++;
-    if (pattern.isAlive(cell.downNeighbour())) livingNeighbours++;
-    if (pattern.isAlive(cell.leftDownNeighbour())) livingNeighbours++;
-    if (pattern.isAlive(cell.upNeighbour())) livingNeighbours++;
-    if (pattern.isAlive(cell.rightUpNeighbour())) livingNeighbours++;
-    return livingNeighbours
-}
+export const countLivingNeighbours = (cell, pattern) => [
+    pattern.isAlive(cell.leftNeighbour()),
+    pattern.isAlive(cell.leftUpNeighbour()),
+    pattern.isAlive(cell.rightNeighbour()),
+    pattern.isAlive(cell.rightDownNeighbour()),
+    pattern.isAlive(cell.downNeighbour()),
+    pattern.isAlive(cell.leftDownNeighbour()),
+    pattern.isAlive(cell.upNeighbour()),
+    pattern.isAlive(cell.rightUpNeighbour())]
+    .reduce((acc, cur) => cur ? acc + 1 : acc, 0)
 
 export const stayAlive = (cell, pattern) => {
     const livingNeighbours = countLivingNeighbours(cell, pattern)

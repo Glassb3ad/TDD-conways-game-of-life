4f8e9b56453578afaf58774967531c96780246da
GlassB3ad <j.lahtivirta@gmail.com>
2024-05-06 21:09:04 +0300

    command line operations

--- src/main.mjs
+++ src/main.mjs
@@ -7,3 +7,8 @@ export const main = (filePath, steps) => {
     const newPattern = generationN(pattern, steps)
     Writer.writeRLE(`${filePath.slice(0, filePath.length - 4)}_result`, newPattern);
 }
+
+const filePath = process.argv[2]
+const steps = Number.parseInt(process.argv[3])
+if (!(filePath && steps)) return "Must give both file path and steps"
+main(filePath, steps)
